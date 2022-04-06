74fb9c94738ebb627a59ef694eb520c6debf041f
JeWelCrax <40703191+JeWelCrax@users.noreply.github.com>
2022-04-06 02:12:31 +0200

    Updated

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const init = () => {
+  const projects = document.getElementById("projects");
+  const contact = document.getElementById("contact");
+
+  projects.onclick = () => {
+    alert("Coming soon :)");
+  };
+
+  contact.onclick = () => {
+    alert("Coming soon :)");
+  };
+};
+
+window.onload = init;
