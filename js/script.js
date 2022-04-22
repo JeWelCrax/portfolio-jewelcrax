ea62817bddd17a64f657ee77716e2d553c98607f
JeWelCrax <40703191+JeWelCrax@users.noreply.github.com>
2022-04-22 22:16:35 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,6 @@
 "use strict";
 
 const init = () => {
-	alert(
-		"IMPORTANT!\n\nThis website is constanly updating, so it may be possible that some pages, animations or transitions are not yet visible/available."
-	);
-
-	alert("Enjoy your visit!");
-
 	const projects = document.getElementById("projects");
 	const contact = document.getElementById("contact");
 
