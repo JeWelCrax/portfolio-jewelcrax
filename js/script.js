008c837a3de8d1500b42b6684ed3ed30ed5bc233
JeWelCrax <40703191+JeWelCrax@users.noreply.github.com>
2022-05-01 19:37:54 +0200

    Adding Documentation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 "use strict";
 
+// Listening for a click event on the projects and contact elements. When the event is triggered, it
+// will run the code inside the function.
 const init = () => {
+	// Getting the elements with the id of projects and contact.
 	const projects = document.getElementById("projects");
 	const contact = document.getElementById("contact");
 
+	// An event listener. It is listening for a click event on the projects and contact elements. When the
+	// event is triggered, it will run the code inside the function.
 	projects.onclick = () => {
 		alert("Coming soon :)");
 	};
@@ -13,4 +18,5 @@ const init = () => {
 	};
 };
 
+// Running the init function when the page loads.
 window.onload = init;
